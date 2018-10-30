c5ebceddab9f7ea64e71684bdb4928a1119e81a5
Croft <tatyenedantas@gmail.com>
2018-10-30 14:18:11 -0300

    "Criando tabuada com for e tabela html"

--- Modulo 04/for.php
+++ Modulo 04/for.php
@@ -22,6 +22,32 @@
         echo $i . "<br/>";
       }
     ?>
+
+    <h3>Tabuada com For</h3>
+
+    <table border>
+
+      <tr>
+        <?php for($i = 1; $i <= 10; $i++) : ?> <!--abriu o for-->
+          <td colspan="2">  <?= $i; ?>  </td>  <!-- colunas  -->
+        <?php endfor; ?>                       <!--Fechou o for (usando php)-->
+      </tr>
+
+      <!--Criando uma tabuada -->
+
+      <?php
+        //No primeiro FOR é para criar as linhas
+        for($i = 1; $i <= 10; $i++){
+            echo "<tr>";
+                for( $j = 1; $j <= 10; $j++ ) { //Cria 20 colunas para cada linha (duas a cada passada no for)
+                  echo "<td width='35px'>" . $j . "*" . $i . " </td>
+                        <td witdh='35px'>" . $j * $i . "</td>";
+                }
+            echo "</tr>";
+        }
+    ?>
+    
+    </table>
   </body>
 
 </html>
