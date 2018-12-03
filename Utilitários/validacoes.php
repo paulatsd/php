13f33e038bf6d4c831cec61990cb9b201b03cf51
Paula Tatyene <paulatatyene@unilab.edu.br>
2018-12-03 16:24:37 -0300

    Validacao de e-mail lado do servidor

---
+++ Utilitários/validacoes.php
@@ -0,0 +1,12 @@
+<?php
+
+class Validacoes{
+
+  // Validar e-mail
+  $email = "croft@gmail.com";
+  $validado = filter_var($email, FILTER_VALIDATE_EMAIL]); //função responsável por validar uma variável passada, de acordo com o tipo de validação desejada (expressão regular)
+
+}
+
+
+ ?>
